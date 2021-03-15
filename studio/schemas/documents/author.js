2d87bb684ca345bfae7b80d8c276f92a778c8e91
graemetg <graemetg@gmail.com>
2021-03-15 14:51:30 +0000

    added field Title to Author schema

--- studio/schemas/documents/author.js
+++ studio/schemas/documents/author.js
@@ -9,6 +9,11 @@ export default {
       title: 'Name'
     },
     {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
